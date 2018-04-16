23c32ce3c5d2990324e08ba43b5ad33592970fb3
Hyman_Chen <hyman_chen@wistron.com>
2018-04-16 17:18:23 +0800

    add firebase and connect to state

---
+++ firebase/env.sample.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAAJI6707ASEXMcg",
+  authDomain: "realti9238.firebaseapp.com",
+  databaseURL: "https://realt9238.firebaseio.com",
+  projectId: "realtim479238",
+  storageBucket: "realti9238.appspot.com",
+  messagingSenderId: "532699"
+};
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import config from '../../firebase/env';
 
 export default class Main extends Component {
+  state = {
+    balls: []
+  }
+  componentDidMount() {
+    firebase.initializeApp(config);
+    this.database = firebase.firestore();
+    const ballCollection = this.database.collection('ball');
+    ballCollection.onSnapshot(d => this.setState({ balls: d.docs }));
+  }
+
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
         <h2>Hello, I`m ReactMaker.</h2>
         <p>To get started, edit containers/index.js</p>
+        {this.state.balls.map(ball =>
+          <div key={ball.id}>{JSON.stringify(ball.data())}</div>)
+        }
       </div>
     );
   }
