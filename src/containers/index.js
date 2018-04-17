0c28a36a6e2f3c7f70fa7223d10ef818e67fb4be
chnbohwr <chnbohwr@gmail.com>
2018-04-17 17:51:41 +0800

    add facebook login

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import config from '../../firebase/env';
 import rgbHex from 'rgb-hex';
+import * as Styled from './style';
+import config from '../../firebase/env';
 
 const rc = () => (Math.floor(Math.random() * 50) + 140);
 
 export default class Main extends Component {
   state = {
     balls: [],
-    users: [],
+    user: {},
   }
-
   componentDidMount() {
     window.firebase = firebase;
     firebase.initializeApp(config);
     this.database = firebase.firestore();
     this.ballCollection = this.database.collection('ball');
     this.ballCollection.onSnapshot(d => this.setState({ balls: d.docs }));
+    firebase.auth().onAuthStateChanged(this.authChange);
+  }
+
+  authChange = (user) => {
+    if (user === null) {
+      this.setState({ user: {} });
+    } else {
+      this.setState({ user });
+    }
+
   }
 
   addBall = () => {
+    const r = Math.floor(Math.random() * 40) + 10;
+    const cx = r + Math.floor(Math.random() * (500 - r));
+    const cy = r + Math.floor(Math.random() * (500 - r));
+    const fill = `#${rgbHex(rc(), rc(), rc())}`;
     const ball = {
-      cx: Math.floor(Math.random() * 500),
-      cy: Math.floor(Math.random() * 500),
-      r: Math.floor(Math.random() * 40) + 10,
-      fill: `#${rgbHex(rc(), rc(), rc())}`
+      cx, cy, r, fill,
     };
     this.ballCollection.add(ball);
   }
@@ -36,19 +47,33 @@ export default class Main extends Component {
 
   renderBall = (ball) => {
     const data = ball.data();
-    return <circle onClick={()=>{this.removeBall(ball)}} key={data.id} cx={data.cx} cy={data.cy} r={data.r} fill={data.fill} />
+    return <circle onClick={() => { this.removeBall(ball); }} key={ball.id} cx={data.cx} cy={data.cy} r={data.r} fill={data.fill} />
+  }
+
+  loginFB = () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  logout = () => {
+    firebase.auth().signOut();
   }
 
   render() {
     return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <button style={{width: 50, height: 50}} onClick={this.addBall}>add ball</button>
+      <Styled.ContainerView>
+        <button style={{ width: 50, height: 50 }} onClick={this.addBall}>add ball</button>
+        {
+          this.state.user.uid ?
+            <Styled.BtnLogin onClick={this.logout}>Logout {this.state.user.displayName}</Styled.BtnLogin> :
+            <Styled.BtnLogin onClick={this.loginFB}>Connect with Facebook</Styled.BtnLogin>
+        }
         <svg width="500" height="500">
           {
             this.state.balls.map(this.renderBall)
           }
         </svg>
-      </div>
+      </Styled.ContainerView>
     );
   }
 }
---
+++ src/containers/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const BtnLogin = styled.button`
+  background: #3b5998;
+  border: 0;
+  border-radius: 100px;
+  color: white;
+  font-size: 16px;
+  padding: 6px 12px;
+  cursor: pointer;
+  width: 200px;
+`;
+
+export const ContainerView = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+
+export const a = 1;
